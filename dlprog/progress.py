03eb76128636d64af170749b95adc8dcd81438ac
misya11p <miya33ps@gmail.com>
2023-07-10 23:17:03 +0900

    time_format

--- dlprog/progress.py
+++ dlprog/progress.py
@@ -1,4 +1,15 @@
+import time
 from typing import Optional, Union
+
+
+SPM = 60 # seconds per minute
+MPH = 60 # minutes per hour
+def time_format(t: float) -> str:
+    h = t // (MPH * SPM)
+    m = t // MPH
+    s = t % MPH
+    return f'{int(h):>02}:{int(m):>02}:{s:05.2f}'
+
 
 Number = Union[int, float]
 
@@ -13,11 +24,13 @@ class TrainProgress:
         self.width = width
         self.symbol = symbol
 
-    def _draw_bar(self, prop: float):
-        bar_text = (self.symbol * int(self.width * prop)).ljust(self.width)
-        prop_text = f'{int(prop * 100)}%'.rjust(5)
+    def _draw_bar(self):
+        bar_text = (self.symbol * int(self.width * self.prop))
+        bar_text = bar_text.ljust(self.width)
+        prop_text = f'{int(self.prop * 100)}%'.rjust(4)
+        time_text = time_format(self.now_time - self.start_time)
         loss_text = f'loss: {self.epoch_loss / self._loss_den:.4f}'
-        text = f'{self._epoch_text}: {bar_text} {prop_text} | {loss_text}'
+        text = f'{self._epoch_text}: {bar_text} {prop_text} | {time_text} | {loss_text}'
         print(text, end='\r', flush=True)
 
     def _make_epoch_text(self):
@@ -29,9 +42,12 @@ class TrainProgress:
 
     def _epoch_start(self):
         self.now_iter = 0
+        self.prop = 0.
         self.epoch_loss = 0
         self.now_epoch += 1
         self._loss_den = 0
+        self.start_time = time.time()
+        self.now_time = self.start_time
         self._make_epoch_text()
 
     def start(self, n_epochs: Optional[int] = None):
@@ -44,8 +60,9 @@ class TrainProgress:
         if loss is not None:
             self.epoch_loss += weight * loss
             self._loss_den += weight
-        prop = min(self.now_iter / self.n_iter, 1)
-        self._draw_bar(prop)
+        self.prop = min(self.now_iter / self.n_iter, 1)
+        self.now_time = time.time()
+        self._draw_bar()
 
     def step(self):
         print()
