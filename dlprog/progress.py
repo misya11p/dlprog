aca0c2a48483180438e4d12ac3abf9e89e1139ab
misya11p <miya33ps@gmail.com>
2023-07-11 02:38:47 +0900

    update

--- dlprog/progress.py
+++ dlprog/progress.py
@@ -8,7 +8,7 @@ def time_format(t: float) -> str:
     h = t // (MPH * SPM)
     m = t // MPH
     s = t % MPH
-    return f'{int(h):>02}:{int(m):>02}:{s:05.2f}'
+    return f'{int(h):02}:{int(m):02}:{s:05.2f}'
 
 
 Number = Union[int, float]
@@ -16,22 +16,34 @@ Number = Union[int, float]
 class TrainProgress:
     def __init__(
         self,
-        n_iter: int,
+        n_iter: int = None,
+        n_epochs: int = None,
         width: int = 50,
-        symbol: str = '#'
+        symbol: str = '#',
     ):
         self.n_iter = n_iter
+        self.n_epochs = n_epochs
         self.width = width
         self.symbol = symbol
+        self.reset()
 
-    def _draw_bar(self):
-        bar_text = (self.symbol * int(self.width * self.prop))
-        bar_text = bar_text.ljust(self.width)
-        prop_text = f'{int(self.prop * 100)}%'.rjust(4)
-        time_text = time_format(self.now_time - self.start_time)
-        loss_text = f'loss: {self.epoch_loss / self._loss_den:.4f}'
-        text = f'{self._epoch_text}: {bar_text} {prop_text} | {time_text} | {loss_text}'
-        print(text, end='\r', flush=True)
+    def set(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def reset(self):
+        self.is_training = False
+        self.now_epoch = 1
+        self._epoch_reset()
+
+    def _epoch_reset(self):
+        self.now_iter = 0
+        self.prop = 0.
+        self.epoch_loss = 0
+        self._loss_den = 0
+        self.start_time = time.time()
+        self.now_time = self.start_time
+        self._make_epoch_text()
 
     def _make_epoch_text(self):
         epoch_text = str(self.now_epoch)
@@ -40,30 +52,47 @@ class TrainProgress:
             epoch_text += f'/{self.n_epochs}'
         self._epoch_text = epoch_text
 
-    def _epoch_start(self):
-        self.now_iter = 0
-        self.prop = 0.
-        self.epoch_loss = 0
-        self.now_epoch += 1
-        self._loss_den = 0
-        self.start_time = time.time()
-        self.now_time = self.start_time
-        self._make_epoch_text()
+    def start(self, **kwargs):
+        self.set(**kwargs)
+        assert self.n_iter is not None, '"n_iter" is not set.'
+        self.is_training = True
+        self.now_epoch = 1
+        self._epoch_reset()
 
-    def start(self, n_epochs: Optional[int] = None):
-        self.now_epoch = 0
-        self.n_epochs = n_epochs
-        self._epoch_start()
+    def _draw_bar(self):
+        bar_text = (self.symbol * int(self.width * self.prop))
+        bar_text = bar_text.ljust(self.width)
+        prop_text = f'{int(self.prop * 100)}%'.rjust(4)
+        time_text = time_format(self.now_time - self.start_time)
+        loss_text = f'loss: {self.epoch_loss / self._loss_den:.4f}'
+        text = ' | '.join([
+            f'{self._epoch_text}: {bar_text} {prop_text}',
+            time_text,
+            loss_text
+        ])
+        print(text, end='\r', flush=True)
 
-    def update(self, loss: Optional[float] = None, weight: Number = 1):
-        self.now_iter += 1
+    def update(
+        self,
+        loss: Optional[float] = None,
+        weight: Number = 1,
+        advance: int = 1,
+        auto_step: bool = True,
+    ):
+        if not self.is_training:
+            self._start()
+
+        self.now_iter += advance
         if loss is not None:
             self.epoch_loss += weight * loss
             self._loss_den += weight
         self.prop = min(self.now_iter / self.n_iter, 1)
         self.now_time = time.time()
         self._draw_bar()
+        if self.now_iter >= self.n_iter and auto_step:
+            self.step()
 
     def step(self):
         print()
-        self._epoch_start()
+        self.now_epoch += 1
+        self._epoch_reset()
