2f643a0d3dcd2dee74d8fd1633373c201b8b1d40
misya11p <miya33ps@gmail.com>
2023-07-11 12:56:51 +0900

    agg_fn

--- dlprog/progress.py
+++ dlprog/progress.py
@@ -1,5 +1,5 @@
 import time
-from typing import Optional, Union
+from typing import Optional, Union, Callable
 
 
 SPM = 60 # seconds per minute
@@ -18,31 +18,46 @@ class Progress:
         self,
         n_iter: int = None,
         n_epochs: int = None,
+        agg_fn: Union[str, Callable[[Number, Number], Number]] = 'mean',
         label: str = None,
         width: int = 40,
         symbol: str = '#',
     ):
         self.n_iter = n_iter
         self.n_epochs = n_epochs
+        self.agg_fn = agg_fn
         self.label = label
         self.width = width
         self.symbol = symbol
         self.reset()
 
+    _agg_fns = {
+        'mean': lambda s, w: s / w,
+        'sum': lambda s, w: s,
+    }
+
+    def _set_agg_fn(self):
+        if isinstance(self.agg_fn, str):
+            self._agg_fn = self._agg_fns[self.agg_fn]
+        else:
+            self._agg_fn = self.agg_fn
+
     def set(self, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v)
+        self._set_agg_fn()
 
     def reset(self):
         self.is_training = False
         self.now_epoch = 1
         self._epoch_reset()
+        self._set_agg_fn()
 
     def _epoch_reset(self):
         self.now_iter = 0
         self.prop = 0.
-        self.epoch_loss = 0
-        self._loss_den = 0
+        self.epoch_value = 0
+        self._epoch_value_weight = 0
         self.start_time = time.time()
         self.now_time = self.start_time
         self._make_epoch_text()
@@ -68,7 +83,8 @@ class Progress:
         prop_text = f'{int(self.prop * 100)}%'.rjust(4)
         time_text = f'[{time_format(self.now_time - self.start_time)}]'
         value_text = f'{self.label}: ' if self.label else ''
-        value_text += f'{self.epoch_loss / self._loss_den:.4f}'
+        value = self._agg_fn(self.epoch_value, self._epoch_value_weight)
+        value_text += f'{value:.4f}'
         text = ' '.join([
             index_text,
             bar_text,
@@ -90,8 +106,8 @@ class Progress:
 
         self.now_iter += advance
         if loss is not None:
-            self.epoch_loss += weight * loss
-            self._loss_den += weight
+            self.epoch_value += weight * loss
+            self._epoch_value_weight += weight
         self.prop = min(self.now_iter / self.n_iter, 1)
         self.now_time = time.time()
         self._draw_bar()
